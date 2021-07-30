66bb0b2983ddf422e3ada8ee79a18edc2bbc7856
KaustubhSathe <f2015466p@alumni.bits-pilani.ac.in>
2021-07-30 17:49:09 +0530

    added .env files and protected password

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-module.exports.connectionURL = "mongodb+srv://kaustubh:EddR97Ut0t5JjyGj@torrentsdb.2tef1.mongodb.net/Production?retryWrites=true&w=majority";
+module.exports.connectionURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@torrentsdb.2tef1.mongodb.net/Production?retryWrites=true&w=majority`;
